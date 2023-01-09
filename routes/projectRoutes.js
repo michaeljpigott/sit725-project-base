68363352497dfca71cf199830659b3631a1bbb40
Maia Coghlan <34679505+MaiaC@users.noreply.github.com>
2023-01-09 19:49:45 +1100

    render html on routes
    add functions for item history to MVC and set up functionality to render html files on different routes

---
+++ routes/projectRoutes.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var router = express.Router();
+let controller = require("../controller");
+let rootDir = process.cwd(); // the root directory of the project on your local computer
+
+// this renders the history.html file on the /history route
+// this will get all the user items to display on the page
+router.get("/history", (req, res) => {
+  //   controller.projectController.retrieveItems(req, res);
+  let fileLocation = rootDir + "/public/history.html"; // creates absolute path for html file
+  res.sendFile(fileLocation);
+});
+
+// this should save a new item to user history
+// router.post("/history", (req, res) => {
+//   controller.projectController.createItem(req, res);
+// });
+
+// this should delete an item from user history
+// router.delete("/history", (req, res) => {
+//   controller.projectController.deleteItem(req, res);
+// });
+
+module.exports = router;
