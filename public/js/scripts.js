4fdbc54fb8d8826dbc592f4f61a58a2bebb047f6
michaeljpigott <35479648+michaeljpigott@users.noreply.github.com>
2023-01-10 17:13:35 +1100

    Commit no.2
    Added css rule
    Updated script.js
    Updated location page.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,16 +1,18 @@
 //function for location autocomplete imput field
 
 $(document).ready(function () {
+  let councilData = {
+    Apple: null,
+    Microsoft: null,
+    Google: "https://placehold.it/250x250",
+  };
   $("input.autocomplete").autocomplete({
-    data: {
-      Apple: null,
-      Microsoft: null,
-      Google: "https://placehold.it/250x250",
-    },
+    data: councilData,
     onAutocomplete: function () {
       let suburbOutput = document.getElementById("suburb-output");
+      let suburbInfo = document.getElementById("suburb-info");
+      suburbInfo.style.display = "inline";
       suburbOutput.innerText = document.getElementById("suburb").value;
     },
   });
 });
-
