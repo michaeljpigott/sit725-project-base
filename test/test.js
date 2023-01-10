385e97efcce9437ce83f18d2d774779c1f36ec6a
michaeljpigott <35479648+michaeljpigott@users.noreply.github.com>
2023-01-10 17:37:26 +1100

    Commit 3
    Created test for location page
    changed name in package.json file (it didn't like the format)

--- test/test.js
+++ test/test.js
@@ -1,16 +1,13 @@
 var expect = require("chai").expect; //chai is an assertion library https://www.youtube.com/watch?v=sPyb6QlgBaU
 var request = require("request");
 
-/*describe("Add Two Numbers", function () {
-  var url = "http://localhost:3000/addTwoNumbers/3/5";
-  it("returns status 200 to check if api works", function (done) {
+//Tests for location feature
+describe("Location page", function () {
+  var url = "http://localhost:3000/location";
+  it("returns status 200 to check if location page works", function (done) {
     request(url, function (error, response, body) {
       expect(response.statusCode).to.equal(200);
       done();
     });
-
-
   });
 });
-
-*/
