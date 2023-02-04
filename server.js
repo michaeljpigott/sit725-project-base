36098785b1d41d689606a1958584553d5bc1466e
michaeljpigott <35479648+michaeljpigott@users.noreply.github.com>
2023-02-04 11:31:39 +1100

    Update server.js
    Removed references to env module

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-var express = require('express');
-var env = require('dotenv').config();
+var express = require("express");
+var env = require("dotenv").config();
 var port = process.env.port || 3000;
-var ejs = require('ejs');
-var path = require('path');
+var ejs = require("ejs");
+var path = require("path");
 var app = express();
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var session = require('express-session');
-var MongoStore = require('connect-mongo')(session);
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var session = require("express-session");
+var MongoStore = require("connect-mongo")(session);
 
 var cors = require("cors");
 let client = require("./dbConnect");
@@ -17,51 +17,54 @@ let historyRoutes = require("./routes/historyRoutes");
 app.use(cors());
 app.use(projectRoutes);
 app.use("/api/history", historyRoutes);
-//app.use("/api/suburbs", projectRoutes); /// might be an issue
 
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 
-
-mongoose.connect('mongodb+srv://canurecycleit:SIT725@cluster0.oqdjdva.mongodb.net/?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}, (err) => {
-  if (!err) {
-    console.log('MongoDB Connection Succeeded.');
-  } else {
-    console.log('Error in DB connection : ' + err);
+mongoose.connect(
+  "mongodb+srv://canurecycleit:SIT725@cluster0.oqdjdva.mongodb.net/?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (!err) {
+      console.log("MongoDB Connection Succeeded.");
+    } else {
+      console.log("Error in DB connection : " + err);
+    }
   }
-});
+);
 
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-});
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {});
 
-app.use(session({
-  secret: 'work hard',
-  resave: true,
-  saveUninitialized: false,
-  store: new MongoStore({
-    mongooseConnection: db
+app.use(
+  session({
+    secret: "work hard",
+    resave: true,
+    saveUninitialized: false,
+    store: new MongoStore({
+      mongooseConnection: db,
+    }),
   })
-}));
+);
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + "/views"));
 
-var index = require('./routes/userRoutes');
-app.use('/', index);
+var index = require("./routes/userRoutes");
+app.use("/", index);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  var err = new Error('File Not Found');
+  var err = new Error("File Not Found");
   err.status = 404;
   next(err);
 });
@@ -73,7 +76,6 @@ app.use(function (err, req, res, next) {
   res.send(err.message);
 });
 
-
 app.listen(port, () => {
-  console.log("App listening to http://localhost:"+port+'/index')
+  console.log("App listening to http://localhost:" + port + "/index");
 });
