0de6b9605edcbf6ac8f267106a793e37e9e4fef3
Maia Coghlan <34679505+MaiaC@users.noreply.github.com>
2023-02-05 20:31:22 +1100

    remove unnecessary code files

--- models/project.js
+++ models/project.js
@@ -1,29 +1,29 @@
-let mongo = require("mongodb");
-let client = require("../dbConnect");
-let historyCollection;
+// let mongo = require("mongodb");
+// let client = require("../dbConnect");
+// let historyCollection;
 
-// connects to database collection called Items (ie user's history of uploaded items)
-setTimeout(() => {
-  historyCollection = client.db().collection("Items");
-}, 2000);
+// // connects to database collection called Items (ie user's history of uploaded items)
+// setTimeout(() => {
+//   historyCollection = client.db().collection("Items");
+// }, 2000);
 
-// this will save a new item to user history
-// const insertItem = (project, callback) => {
-//   historyCollection.insert(project, callback);
-// };
+// // this will save a new item to user history
+// // const insertItem = (project, callback) => {
+// //   historyCollection.insert(project, callback);
+// // };
 
-// this will get all items from user history
-// const getItems = (callback) => {
-//   historyCollection.find({}).toArray(callback);
-// };
+// // this will get all items from user history
+// // const getItems = (callback) => {
+// //   historyCollection.find({}).toArray(callback);
+// // };
 
-// this will delete an item from user history
-// const removeItem = (itemId, callback) => {
-//   historyCollection.remove({ _id: new mongo.ObjectId(itemId) }, callback);
-// };
+// // this will delete an item from user history
+// // const removeItem = (itemId, callback) => {
+// //   historyCollection.remove({ _id: new mongo.ObjectId(itemId) }, callback);
+// // };
 
-// module.exports = {
-//   insertItem,
-//   getItems,
-//   removeItem,
-// };
+// // module.exports = {
+// //   insertItem,
+// //   getItems,
+// //   removeItem,
+// // };
